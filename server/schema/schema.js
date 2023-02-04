4ccdd7e25e19b8b08a5105f263462e4b3be81707
sridharawepay <sridhar@awepay.com>
2023-02-05 01:43:18 +0800

    update graphQL queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,15 +9,21 @@ var usersData = [
     {id: '211', name: 'Bella', age: 16, profession: "Programmer"},
     {id: '19', name: 'Gina', age: 26, profession: "Data Manager"},
     {id: '150', name: 'Georgina', age: 36, profession: "Baker"}
-
 ];
 
 var hobbyData = [
-    {id: '1', title: 'Programming', description: "Software Engineer"},
-    {id: '2', title: 'Anna', description: "Civil Engineer"},
-    {id: '3', title: 'Bella', description: "Programmer"},
-    {id: '4', title: 'Gina', description: "Data Manager"},
-    {id: '5', title: 'Georgina', description: "cooking"}
+    {id: '1', title: 'Programming', description: "Software Engineer", userId: '1'},
+    {id: '2', title: 'Anna', description: "Civil Engineer", userId: '13'},
+    {id: '3', title: 'Bella', description: "Programmer", userId: '19'},
+    {id: '4', title: 'Gina', description: "Data Manager", userId: '19'},
+    {id: '5', title: 'Georgina', description: "cooking", userId: '150'}
+
+];
+
+var postData = [
+    {id: '1', comment: "Building a Mind", userId: '1'},
+    {id: '2', comment: "GraphQL is Amazing", userId: '1'},
+    {id: '3', comment: "Change the world 1", userId: '19'},
 
 ];
 
@@ -37,7 +43,21 @@ const UserType = new GraphQLObjectType({
         id: {type: GraphQLString},
         name: {type: GraphQLString},
         age: {type: GraphQLInt},
-        profession: {type: GraphQLString}
+        profession: {type: GraphQLString},
+
+        posts: {
+            type: new graphql.GraphQLList(PostType),
+            resolve(parent, args) {
+                return _.filter(postData, {userId: parent.id});
+            }
+        },
+
+        hobbies : {
+            type: new graphql.GraphQLList(HobbyType),
+            resolve(parent, args) {
+                return _.filter(hobbyData, {userId: parent.id});
+            }
+        }
     })
 });
 
@@ -47,7 +67,30 @@ const HobbyType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         title: {type: GraphQLString},
-        description: {type: GraphQLString}
+        description: {type: GraphQLString},
+        user: {
+            type: UserType,
+            resolve(parent, args) {
+                return _.find(usersData, {id: parent.userId})
+            }
+        }
+    })
+});
+
+//POST Type (id, comment)
+
+const PostType = new GraphQLObjectType({
+    name: 'Post',
+    description: 'Post description',
+    fields: () => ({
+        id: {type: GraphQLID},
+        comment: {type: GraphQLString},
+        user: {
+            type: UserType,
+            resolve(parent, args) {
+                return _.find(usersData, {id: parent.userId})
+            }
+        }
     })
 })
 
@@ -86,7 +129,18 @@ const RootQuery = new GraphQLObjectType({
                 return _.find(hobbyData, {id: args.id})
                 //return data for our hobby
             }
+        },
+
+        post: {
+            type: PostType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                //return data (post data)
+                return _.find(postData, {id: args.id})
+            }
         }
+
+
     }
 });
 
